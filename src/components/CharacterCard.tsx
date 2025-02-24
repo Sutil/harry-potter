2ce2bf53b7565d9ea5b6f457f3c9f75bbf03eff2
Eduardo Sutil <sutil.edu@gmail.com>
2025-02-24 16:18:19 -0300

    Students page

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -5,7 +5,7 @@ export const CharacterCard: React.FC<{ character: Character }> = ({
   character,
 }) => {
   return (
-    <div className="w-full p-5 rounded-lg shadow-md hover:shadow-lg">
+    <div className="w-full p-5 rounded-lg shadow-md hover:shadow-lg border">
       <Avatar className="w-full h-52 rounded-md object-cover">
         <AvatarImage
           className="max-w-full max-h-full w-auto h-auto"
@@ -16,11 +16,7 @@ export const CharacterCard: React.FC<{ character: Character }> = ({
           <span className="text-xs">{character.name}</span>
         </AvatarFallback>
       </Avatar>
-      {/* <img
-        src={character.image}
-        alt={character.name}
-        className="w-full h-52 rounded-md object-cover"
-      /> */}
+
       <div className="mt-2">
         <h2 className="h-10 leading-4 text-md font-bold">{character.name}</h2>
         <p className="text-sm text-gray-600 h-6">{character.house}</p>
